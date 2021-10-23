55a4b45d8a0a65cdfeb5f4e3e9ef3db79671e5a4
callmejogi <kutafin01@gmail.com>
2021-10-23 14:03:19 +0300

    final

--- js/script.js
+++ js/script.js
@@ -3,28 +3,18 @@ const arr = [1,2,3,-1,-2,-3];
 function getArray(arr){
     const positiveArr = [];
 
-    if(arr.length === 0){
-        return console.log(`Array is empty!`);
-    };
+    if(arr.length === 0) return console.log(`Array is empty!`);
 
-    for(i = 0 ; i < arr.length;i++){
-        if(arr[i] > 0){
-            positiveArr.push(arr[i]);
-        }
-    };
+    for(let i = 0 ; i < arr.length;i++){
+        if(arr[i] > 0) positiveArr.push(arr[i]);
+    }
 
-    if(positiveArr.length === 0){
-        return console.log(null);
-    }else{
-        return console.log(positiveArr);
-    };
+    return positiveArr.length === 0 ? null : positiveArr;
+
 }
 
-getArray(arr);
+console.log(getArray(arr));
 
-//фрагмент задания по желанию 
-//let check = function checkArray(arr){
-//    if (typeof arr.forEach() !== "number"){
-//        return console.log(`Array should consist of numbers only!`);
-//    }
+//function isValidNumber(arg){
+//    if (arg !== "number" && isNaN(arg));
 //}
